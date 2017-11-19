8631f42f4eb362105dc1a2cee71339df9c09e966
danielarnarsson <danielar216@nemi.tskoli.is>
2017-11-19 23:29:20 +0000

    Imported os and created port

--- README.md
+++
@@ -1,1 +0,0 @@
-# daniel-verkefni-12B
---
+++ app.py
@@ -0,0 +1,57 @@
+from bottle import route, run, request, get, post, template
+import pymysql.cursors, os
+
+@route('/', method='get')
+def signUpOrLogin():
+    return template('index')
+
+@route('/leynisida', method='post')
+def signedin():
+	u = request.forms.get('user')
+	p = request.forms.get('pass')
+	connection = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1907002160', passwd='mypassword', db='1907002160_verkefni10')
+	cur = connection.cursor()
+	sql = "SELECT `user`,`pass` FROM `user` WHERE `user` = %s  AND `pass` = %s"
+	cur.execute(sql, (u,p))
+	acc=cur.fetchone()
+	try:
+		user=acc[0]
+		passw=acc[1]
+
+	except TypeError:
+		user=None
+		passw=None
+
+	if user != None or passw != None:
+		if u==user and p==passw: 
+			return template('leynisida.tpl')
+	else:
+		return "rangt notendanafn og/eða lykilorð"
+
+@route('/result', method='post')
+def nyr():
+	u = request.forms.get('user')
+	p = request.forms.get('pass')
+	connection = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1907002160', passwd='mypassword', db='1907002160_verkefni10')
+	cur = connection.cursor()
+	sql = "SELECT `user` FROM `user` WHERE `user` LIKE %s"
+	cur.execute(sql, u)
+	acc = cur.fetchone()
+	try:
+		user = acc[0]
+	except TypeError:
+		user=None
+
+	if user==u:
+		return "Notendanafn er núþegar til"
+	else:
+		connection = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1907002160', passwd='mypassword', db='1907002160_verkefni10')
+		cur = connection.cursor()
+		cur.execute("INSERT INTO `user` (`user`, `pass`) VALUES (%s, %s)", (u,p))
+		connection.commit()
+		return 'Notandi skráður'
+
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+    run(host='0.0.0.0', port=port, debug=True)
